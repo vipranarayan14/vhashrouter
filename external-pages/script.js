268f6b6b113c19f0c6635bcc0727c166946e614e
vipranarayan14 <vipranarayan14@gmail.com>
2017-09-25 20:13:40 +0530

    move 'gallery/' to 'external-pages/'.

---
+++ external-pages/script.js
@@ -0,0 +1,5 @@
+function sayHello() {
+
+  document.querySelector('#Internationalized_URL')
+    .insertAdjacentHTML('afterend', '<br><b><i>This text was set dynamically :)</i><b>');
+}
--- gallery/script.js
+++
@@ -1,4 +0,0 @@
-function sayHello() {
-
-  console.log(document.querySelector('.input-text').value = 'This input value was set dynamically :)');
-}
