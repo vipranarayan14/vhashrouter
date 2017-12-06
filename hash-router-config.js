21f75ba13413d7a17a61a33365433f5a4a6d2505
vipranarayan14 <vipranarayan14@gmail.com>
2017-12-06 17:49:47 +0530

    test hashVars. fix eslint errors.

--- hash-router-config.js
+++ hash-router-config.js
@@ -1,36 +1,41 @@
 HashRouter.init({
-  defaultRoute: "#/Intro",
+  defaultRoute: '#/Intro',
   navRoutes: [
     {
-      route: "#/sample-external-page/1",
-      id: "External-page",
-      url: "external-pages/index.html",
+      route: '#/sample-external-page/{num}',
+      id: 'External-page',
+      url: 'external-pages/index.html',
       rsrcs: {
         scripts: ['external-pages/script.js', 'external-pages/script2.js'],
         styles: ['external-pages/style.css']
       },
-      onSuccess: function () {
+      onSuccess: function (varss) {
 
+        console.log(varss);
         runEachTime();
       },
       onFailure: function () {
-        
+
         console.log('Getting external page failed!');
       }
     },
     {
-      route: "#/gallery",
-      id: "gallery",
-      url: "external-pages/gallery.html",
+      route: '#/gallery',
+      id: 'gallery',
+      url: 'external-pages/gallery.html',
     },
     {
-      route: "#/License",
-      id: "License",
+      route: '#/License',
+      id: 'License',
       url: 'LICENSE'
     },
     {
-      route: "#/Introduction",
-      id: "Intro"
+      route: '#/Introduction/{w}/{e}/',
+      id: 'Intro',
+      onSuccess: function (varss) {
+        
+        console.log(varss);
+      }
     }
   ]
 });
