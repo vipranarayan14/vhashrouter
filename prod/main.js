75d4264b7a974fb1bd6d7fce8ce0bd1eadbfaf0c
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2017-12-23 15:37:32 +0530

    add 'init', 'config' and 'options' to HashRouter.

--- prod/main.js
+++ prod/main.js
@@ -1,3 +1,10 @@
-import HashRouter from './variables';
+import initHashRouting from './init';
+import configuration from './configure';
+
+let HashRouter = {};
+
+HashRouter.init = initHashRouting;
+HashRouter.config = configuration.configureOptions;
+HashRouter.options = configuration.options;
 
 window.HashRouter = HashRouter;
