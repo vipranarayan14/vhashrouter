a1dcc3bdcb1b03f568eac809b50454101f3710e1
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2017-12-23 15:36:19 +0530

    make 'makeRegExp' to change even empty curly braces to 'hashParams'.

--- prod/make-regex.js
+++ prod/make-regex.js
@@ -2,9 +2,11 @@ function makeRegExp(str) {
 
   str = str.replace(/[-/\\^$*+?.()|[\]]/g, '\\$&')
     .replace(/\$/g, '$$$$')
-    .replace(/{(.+?)}/g, '(.[^/]*)');
+    .replace(/{(.*?)}/g, '(.[^/]*)');
 
   str = new RegExp(str);
 
   return str;
 }
+
+export default makeRegExp;
