fb379ccb850306d057d507ac64754359ecd9436d
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2017-12-24 17:09:13 +0530

    remove unnecessary comments

--- prod/main.js
+++ prod/main.js
@@ -8,3 +8,27 @@ HashRouter.config = configuration.configureOptions;
 HashRouter._options = configuration.options;
 
 window.HashRouter = HashRouter;
+
+/*
+
+HashRouter.init = config => {
+
+  window.onload = () => {
+
+    hashHandler = () => {};
+
+    removeListener = window.addEventListener('hashchange', () => {});
+
+  }
+
+}
+
+HashRouter.updateConfig = config => {
+
+  tearDown(); // removeListener();
+
+  HashRouter.init(config);
+
+}
+
+*/
