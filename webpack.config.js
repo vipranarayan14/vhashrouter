ffbcea19aff0a09cc9296d9e5e7a4655ce01b180
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2017-12-24 17:42:46 +0530

    rename 'dist' to 'lib'.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'lib'),
     filename: 'hash-router.js'
   },
   module: {
