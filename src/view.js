b39626a355cdea65065c1a2a1a77eb33907c126c
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2017-12-24 19:35:44 +0530

    use 'querySelctor' instead of 'getElementsByClassName' and 'getElementById' for easy iteration using 'forEach'.

--- src/view.js
+++ src/view.js
@@ -1,21 +1,29 @@
-import {activeHashClass, navPageSelector} from './literals';
+import { activeHashClass, navPageSelector } from './literals';
 
 const hideAllViews = () => {
-  const views = document.getElementsByClassName(navPageSelector);
 
-  for (let i = 0, length = views.length; i < length; i++) {
-    views[i].classList.remove(activeHashClass);
-  }
-}
+  const views = document.querySelectorAll(`.${ navPageSelector}`);
+
+  views.forEach(view => {
+
+    view.classList.remove(activeHashClass);
+
+  });
+
+};
 
 const showView = viewId => {
-  document.getElementById(viewId)
+
+  document.querySelector(`#${ viewId}`)
     .classList.add(activeHashClass);
-}
+
+};
 
 export const changeView = (config, routeConfig) => {
+
   hideAllViews();
   showView(routeConfig.targetId);
-  
+
   window.scrollTo(0, 0);
-}
+
+};
