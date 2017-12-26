2d04ce08bbe4cd46502e0853fdf9a74b0e603ffc
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2017-12-26 09:02:30 +0530

    make 'makeRegExp' to add '^' and '$' in the start and end, respectively, of the 'routeConfig.hash' to avoid partial match such as matching 'Intro' to 'Introduction'.

--- src/make-regex.js
+++ src/make-regex.js
@@ -2,7 +2,8 @@ export const makeRegExp = string => {
 
   const expression = string.replace(/[-/\\^$*+?.()|[\]]/g, '\\$&')
     .replace(/\$/g, '$$$$')
-    .replace(/{(.*?)}/g, '(.[^/]*)');
+    .replace(/{(.*?)}/g, '(.[^/]*)')
+    .replace(/^(.*)$/, '^$1$');
 
   return new RegExp(expression);
 
